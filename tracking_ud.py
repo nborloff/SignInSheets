e8dfb3a6f96449fb1cd0ff7899741efbc4862462
Nick Orloff <norloff@mendocino.edu>
2022-02-11 12:58:50 -0800

    pre break

--- tracking_ud.py
+++ tracking_ud.py
@@ -6,17 +6,17 @@ from pprint import pprint
 from re import I, search
 import re
 from datetime import datetime
-import math
 import numpy as np
 
 '''Read Excel Spreadsheet - Only Relevant Columns'''
 df = pd.read_csv('https://api.codereadr.com/share/45d80f27f0f12c8402f2e2371c849990', usecols = ['User Name', 'Barcode', 'Result', 'Timestamp Scanned', 'Answer 1'])
 
+'''Fill in empty cells to avoid later errors'''
 df.rename(columns={'Answer 1': 'Answer'}, inplace=True)
 df1 = df.replace(np.nan, 'Other', regex=True)
-print(df1)
+
 '''Convert Datafram to List'''
-master_list = df.values.tolist()
+master_list = df1.values.tolist()
 
 '''Reverse List'''
 master_list.reverse()
@@ -27,24 +27,16 @@ LLRC = []
 MESA = []
 Fitness = []
 
-'''Separate Data by Location + Formatting'''
+'''Simplify IN/OUT Flag'''
 for location, SID, in_out, t_stamp, LLRC_Prog in master_list:
-    
-    if search("IN", in_out): # Add entry that specifies IN or OUT before formatting
+    if search("IN", in_out): 
         in_out = "IN"
     else:
         in_out = "OUT"
-
     child_list.append([location, SID, in_out, t_stamp, LLRC_Prog])
 
-
-for location, SID, in_out, t_stamp, LLRC_Prog in master_list:
-    
-    str(LLRC_Prog)
-    
-    child_list.append([location, SID, in_out, t_stamp, LLRC_Prog])
-
-for location, SID, in_out, t_stamp, LLRC_Prog in child_list: # sort into lists by location
+'''Sort into separate lists by location'''
+for location, SID, in_out, t_stamp, LLRC_Prog in child_list: 
     if location == "LLRC":
         LLRC.append([location, SID, in_out, t_stamp, LLRC_Prog])
     elif location == "MESA-MC":
@@ -61,26 +53,90 @@ LLRC_df.to_csv('LLRC.csv', index=False)
 MESA_df.to_csv('MESA.csv', index=False)
 Fitness_df.to_csv('Fitness.csv', index=False)
 
-'Declare Lists for LLRC Programs'
+'''Declare Lists for LLRC Programs'''
 Math_Lab = []
 Student_Computers = []
-EDU_500 = []
+Math_EDU_500 = []
+English_EDU_500 = []
 Study_Room = []
+DRC_Testing = []
 Other = []
+#MESA = [FULL OF STUFF]
+#Fitness = [FULL OF STUFF]
 
-'''Separates LLRC by Program'''
-
-'''for location, SID, in_out, t_stamp, LLRC_Prog in LLRC:
-    if search("Lab", LLRC_Prog):
+'''Separates LLRC List by Program'''
+for location, SID, in_out, t_stamp, LLRC_Prog in LLRC:
+    if LLRC_Prog == "Faculty Math Lab: 540":
         Math_Lab.append([SID, t_stamp, LLRC_Prog, in_out])
-    elif search("Computers", LLRC_Prog):
+    elif LLRC_Prog == "Math Lab: 540":
+        Math_Lab.append([SID, t_stamp, LLRC_Prog, in_out])
+    elif LLRC_Prog == "Student Computers":
         Student_Computers.append([SID, t_stamp, LLRC_Prog, in_out])
-    elif search("Peer", LLRC_Prog):
-        EDU_500.append([SID, t_stamp, LLRC_Prog, in_out])
-    elif search("Room", LLRC_Prog):
+    elif LLRC_Prog == "Peer-Tutor Math Tutor: EDU 500":
+        Math_EDU_500.append([SID, t_stamp, LLRC_Prog, in_out])
+    elif LLRC_Prog == "Peer-Tutor English Tutor: EDU 500":
+        English_EDU_500.append([SID, t_stamp, LLRC_Prog, in_out])
+    elif LLRC_Prog == "EDU 500": # we can remove this one next month
+        English_EDU_500.append([SID, t_stamp, LLRC_Prog, in_out])
+    elif LLRC_Prog == "Study Room":
         Study_Room.append([SID, t_stamp, LLRC_Prog, in_out])
+    elif LLRC_Prog == "DRC Testing":
+        DRC_Testing.append([SID, t_stamp, LLRC_Prog, in_out])
     else:
-        Other.append([SID, t_stamp, LLRC_Prog, in_out])'''
+        Other.append([SID, t_stamp, LLRC_Prog, in_out])
+
+Count_List = {}
+
+'''Puts list into dictionary with key as SID and values as timestamps'''
+def to_dict(zeit):
+    for SID, t_stamp, LLRC_Prog, in_out in zeit:
+        if SID not in Count_List:
+            Count_List[SID] = [t_stamp]
+            Count_List[SID].append(in_out)
+        else:
+            Count_List[SID].append(t_stamp)
+            Count_List[SID].append(in_out)
+
+to_dict(Math_Lab)
+# pprint(Count_List)
+'''Calculates number of minutes per session per student - flags students
+who signed in but didn't sign out'''
+
+'''For the following function: Proper output is an integer sandwiched between
+an 'IN' and an 'OUT'. Double 'INs', solo 'INs', and double 'Outs' all signify
+that the student didn't sign out properly and must be dealt with computationally
+according to the wishes of the manager of the department'''
+
+Total_Dict = {}
+
+fmt = '%Y-%m-%d %H:%M:%S'
+def check_valid(Zeit_Worterbuch):
+    temp = ""
+    for key, val in Zeit_Worterbuch.items():
+        for i in val:
+            if i == "IN":
+                Total_Dict[key] += ["IN"]
+            elif i == "OUT":
+                Total_Dict[key] += ["OUT"]    
+            elif key not in Total_Dict:
+                temp = i
+                Total_Dict.update({key: ["IN"]})
+            
+            
+            elif i[0:11] == temp[0:11]:
+                tstamp1 = datetime.strptime(i, fmt)
+                tstamp2 = datetime.strptime(temp, fmt)
+                difference = int(round(abs((tstamp2 - tstamp1).total_seconds()) / 60))      
+                Total_Dict[key] += [difference]
+                
+                
+            elif key in Total_Dict:    
+                
+                temp = i
+                
 
 
-#pprint(Math_Lab)
+check_valid(Count_List)
+
+pprint(Total_Dict)
+
